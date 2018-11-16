717a54725db0b4b68c6bba1b0b8ab521f6b619f7
AllysiaJones <allysia997@gmail.com>
2018-11-16 18:38:26 -0500

    stores movie and pre-sale movies

---
+++ main.js
@@ -0,0 +1,90 @@
+document.getElementById("avatar").onclick = function () {
+    localStorage.setItem("movie", "Avatar");
+}
+document.getElementById("titanic").onclick = function () {
+    localStorage.setItem("movie", "Titanic");
+}
+document.getElementById("starWars").onclick = function () {
+    localStorage.setItem("movie", "Star Wars: The Force Awakens");
+}
+document.getElementById("avengers").onclick = function () {
+    localStorage.setItem("movie", "Avengers: Infinity War");
+}
+document.getElementById("jurassic").onclick = function () {
+    localStorage.setItem("movie", "Jurassic World");
+}
+document.getElementById("furious").onclick = function () {
+    localStorage.setItem("movie", "Furious 7");
+}
+document.getElementById("blackpanther").onclick = function () {
+    localStorage.setItem("movie", "Black Panther");
+}
+document.getElementById("harrypotter").onclick = function () {
+    localStorage.setItem("movie", "Harry Potter and the Deathly Hallows: Part 2");
+}
+document.getElementById("frozen").onclick = function () {
+    localStorage.setItem("movie", "Frozen");
+}
+document.getElementById("beauty").onclick = function () {
+    localStorage.setItem("movie", "Beauty and the Beast");
+}
+document.getElementById("incredibles").onclick = function () {
+    localStorage.setItem("movie", "Incredibles 2");
+}
+document.getElementById("minions").onclick = function () {
+    localStorage.setItem("movie", "Minions");
+}
+document.getElementById("transformers").onclick = function () {
+    localStorage.setItem("movie", "Transformers: Dark of the Moon");
+}
+document.getElementById("skyfall").onclick = function () {
+    localStorage.setItem("movie", "Skyfall");
+}
+document.getElementById("darkknight").onclick = function () {
+    localStorage.setItem("movie", "The Dark Knight Rises");
+}
+document.getElementById("toystory3").onclick = function () {
+    localStorage.setItem("movie", "Toy Story 3");
+}
+document.getElementById("pirates").onclick = function () {
+    localStorage.setItem("movie", "Pirates of the Caribbean: Dead Man's Chest");
+}
+document.getElementById("despicableme").onclick = function () {
+    localStorage.setItem("movie", "Despicable Me 3");
+}
+document.getElementById("findingdory").onclick = function () {
+    localStorage.setItem("movie", "Finding Dory");
+}
+document.getElementById("alice").onclick = function () {
+    localStorage.setItem("movie", "Alice in Wonderland");
+}
+document.getElementById("lego").onclick = function () {
+    localStorage.setItem("movie", "The Lego Movie 2: The Second Part");
+}
+document.getElementById("trainyourdragon").onclick = function () {
+    localStorage.setItem("movie", "How to Train Your Dragon: The Hidden World");
+}
+document.getElementById("dumbo").onclick = function () {
+    localStorage.setItem("movie", "Dumbo");
+}
+document.getElementById("aladdin").onclick = function () {
+    localStorage.setItem("movie", "Aladdin");
+}
+document.getElementById("godzilla").onclick = function () {
+    localStorage.setItem("movie", "Godzilla: King of the Monsters");
+}
+document.getElementById("darkphoenix").onclick = function () {
+    localStorage.setItem("movie", "Dark Phoenix");
+}
+document.getElementById("toystory4").onclick = function () {
+    localStorage.setItem("movie", "Toy Story 4");
+}
+document.getElementById("spiderman").onclick = function () {
+    localStorage.setItem("movie", "Spider-Man: Far From Home");
+}
+document.getElementById("it").onclick = function () {
+    localStorage.setItem("movie", "It: Chapter Two");
+}
+document.getElementById("starwars9").onclick = function () {
+    localStorage.setItem("movie", "Star Wars: Episode IX");
+}
